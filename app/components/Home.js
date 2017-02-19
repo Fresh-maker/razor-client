423ff2a6ed77ad154a6321ce1f3f17da2b750340
Tarik <tariktheturk@gmail.com>
2017-02-18 17:19:47 -0800

    Add skeleton.css

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,15 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import skeleton from '../styles/skeleton.css';
 import styles from './Home.css';
 import Window1Page from '../containers/Window1Page';
 import Window2Page from '../containers/Window2Page';
 
-
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container}>
+      <div >
+        <div className="home-page">
           <Window1Page />
           <Window2Page />
         </div>
