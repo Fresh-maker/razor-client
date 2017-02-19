2b782d23414da15e71944053b1f07ffebc436d9b
Tarik <tariktheturk@gmail.com>
2017-02-19 00:16:26 -0800

    Support for markdown

--- app/components/Window2.js
+++ app/components/Window2.js
@@ -3,42 +3,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Text } from 'react-dom';
 import _ from 'underscore';
+import showdown from 'showdown';
 import styles from './Window2.css';
 import ALL_DATA from '../fixtures/alldata.js';
 const ALL_DATA_BY_ID = _.indexBy(ALL_DATA, 'id');
-
-
+const converter = new showdown.Converter();
 
 class Window2 extends Component {
   render() {
-    function linkify(text){
-      let i=0;
-      return (
-        <div className="fullText">
-          {
-            text.split(' ').map(
-              function(word){
-                i++;
-                if (word.indexOf('<Link>') > -1) {
-                  const [,url,text] = word.split('|');
-                  return <Link key={i} to={url}>{text}</Link>
-                } else {
-                  return word+' '
-                }
-              }
-            )
-          }
-        </div>
-
-      );
-    }
-    const fullText = linkify(ALL_DATA_BY_ID[this.props.params.id].fullText);
     const { currentSearch, updateSearch } = this.props;
+    const text = ALL_DATA_BY_ID[this.props.params.id].fullText;
+    const html = converter.makeHtml(text);
     return (
       <div className="window2">
       <div className="row">
         <p>Window2</p>
-        <pre className="fullDetails" >{fullText}</pre>
+        <div className="fullDetails" dangerouslySetInnerHTML={{__html: html}} ></div>
       </div>
 
     </div>
