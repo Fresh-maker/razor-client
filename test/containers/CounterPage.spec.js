6429c1ff41872c6b6da3a426f0e12f5bc5d27e71
Tarik <tariktheturk@gmail.com>
2017-03-06 10:33:51 -0500

    Update tests so that they run
    * stubbed old counter code, left for reference

--- test/containers/CounterPage.spec.js
+++ test/containers/CounterPage.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import CounterPage from '../../app/containers/CounterPage';
+import CounterPage from '../../app/containers/Window1Page';
 import configureStore from '../../app/store/configureStore';
 
 
@@ -22,34 +22,34 @@ function setup(initialState) {
 
 
 describe('containers', () => {
-  describe('App', () => {
-    it('should display initial count', () => {
-      const { p } = setup();
-      expect(p.text()).to.match(/^0$/);
-    });
+  //describe('App', () => {
+    //it('should display initial count', () => {
+      //const { p } = setup();
+      //expect(p.text()).to.match(/^0$/);
+    //});
 
-    it('should display updated count after increment button click', () => {
-      const { buttons, p } = setup();
-      buttons.at(0).simulate('click');
-      expect(p.text()).to.match(/^1$/);
-    });
+    //it('should display updated count after increment button click', () => {
+      //const { buttons, p } = setup();
+      //buttons.at(0).simulate('click');
+      //expect(p.text()).to.match(/^1$/);
+    //});
 
-    it('should display updated count after descrement button click', () => {
-      const { buttons, p } = setup();
-      buttons.at(1).simulate('click');
-      expect(p.text()).to.match(/^-1$/);
-    });
+    //it('should display updated count after descrement button click', () => {
+      //const { buttons, p } = setup();
+      //buttons.at(1).simulate('click');
+      //expect(p.text()).to.match(/^-1$/);
+    //});
 
-    it('shouldnt change if even and if odd button clicked', () => {
-      const { buttons, p } = setup();
-      buttons.at(2).simulate('click');
-      expect(p.text()).to.match(/^0$/);
-    });
+    //it('shouldnt change if even and if odd button clicked', () => {
+      //const { buttons, p } = setup();
+      //buttons.at(2).simulate('click');
+      //expect(p.text()).to.match(/^0$/);
+    //});
 
-    it('should change if odd and if odd button clicked', () => {
-      const { buttons, p } = setup({ counter: 1 });
-      buttons.at(2).simulate('click');
-      expect(p.text()).to.match(/^2$/);
-    });
-  });
+    //it('should change if odd and if odd button clicked', () => {
+      //const { buttons, p } = setup({ counter: 1 });
+      //buttons.at(2).simulate('click');
+      //expect(p.text()).to.match(/^2$/);
+    //});
+  //});
 });
